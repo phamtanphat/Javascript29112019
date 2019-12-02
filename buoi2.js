757804ef2ed7359282bbcc825f18ab3d7c93cb36
phamtanphat <phatdroid94@gmail.com>
2019-12-02 03:55:49 -0800

    Khai bao bien,Kieu du lieu

---
+++ buoi2.js
@@ -0,0 +1,34 @@
+// 1 : Khai bao bien 
+// var teo = "Nguyen Van Teo"
+// teo = 20
+
+// let age = 25
+// age = 30
+
+// const number = 5
+// number = 6
+// console.log(number)
+
+// 2 : Kieu du lieu
+// nguyen thuy : primities
+// 1 - String
+// 2 - Number
+// 3 - Boolean
+// 4 - null
+// 5 - undefined 
+// 6 - symbol
+// doi tuong : object
+
+// null : khi nao gap null
+// var a = null
+// console.log(a)
+// undefined : Khi nao gap
+// Tao ra bien bang tu khoa var , let ma khong gan gia tri
+var a
+console.log(a);
+
+
+
+
+
+
--- test.js
+++
@@ -1,8 +0,0 @@
-// hello
-// Local may
-// Tao ra thu muc .git : git init
-// Xem thay doi : git status
-// Luu thay doi : git add buoi1.js
-// Dua tep luu tru vao trong git : git commit -m "ten commit"
-// Xem lich su commit : git log
-// Down load reposi : git clone https://github.com/phamtanphat/Javascript29112019.git
