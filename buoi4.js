c99bf2ea2fc74260dab91d4ab59a7b75460ee930
phamtanphat <phatdroid94@gmail.com>
2019-12-06 03:58:21 -0800

    array method

--- buoi4.js
+++ buoi4.js
@@ -1,8 +1,19 @@
-var number = 5 //(number)
-var name = "Nguyen Van Teo" //(string)
-var array = [1,2,3,4,5,6,7,8,9,10]//(object)
-var teo = {name : 'Teo', age : 10} //(object)
-var specialundifined = undefined //(undefined)
-var specialNull = null //(object)
-var specailFunction = console.log   //(function)
-console.log(typeof specailFunction)
+// var number = 5 //(number)
+// var name = "Nguyen Van Teo" //(string)
+// var array = [1,2,3,4,5,6,7,8,9,10]//(object)
+// var teo = {name : 'Teo', age : 10} //(object)
+// var specialundifined = undefined //(undefined)
+// var specialNull = null //(object)
+// var specailFunction = console.log   //(function)
+// console.log(typeof specailFunction)
+// 13 – Array method
+const numbers = [1,2,3,4,5]
+// Them phan tu vao vi tri cuoi cung
+// numbers.push(6,7,8)
+// Xoa phan tu vi tri cuoi cung
+// numbers.pop()
+// Them phan tu o vi tri dau tien
+// numbers.unshift(-1,0)
+// Xoa phan tu o vi tri dau tien
+numbers.shift()
+console.log(numbers)
