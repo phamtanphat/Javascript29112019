d460149ccbdc4680e6432f7ccff14c43ec573cf2
phamtanphat <phatdroid94@gmail.com>
2019-12-06 03:12:35 -0800

    btap for in

--- buoi3.js
+++ buoi3.js
@@ -125,15 +125,44 @@
 // for (var key in arrayNames){
 //     console.log(key)
 // }
-const teo = {
-    name : "Nguyen Van Teo",
-    age : 10,
-    address : "Quan 10"
-}
-for (var key in teo){
-    console.log(key)
-}
+// const teo = {
+//     name : "Nguyen Van Teo",
+//     age : 10,
+//     address : "Quan 10"
+// }
+// for (var key in teo){
+//     console.log(key)
+// }
 
+/**
+ * Sử dụng vòng lặp for...in để in ra tất cả các key trong object sau, in ra cả các key của các nested object (object con lồng bên trong object lớn)
+ * Gợi ý: đây là bài tập khó, bạn nên tìm hiểu về recursive trước
+ */
+var apartment = {
+    bedroom: {
+      area: 20,
+      bed: {
+        type: 'twin-bed',
+        price: 100
+      }
+    }
+  };
+  
+  function getkey(object){
+  }
+  
+  getkey(apartment)
+  
+  /**
+   * Kết quả mong muốn:
+   * bedroom
+   * area
+   * bed
+   * type
+   * price
+   * Chú ý: không cần hiển thị ra đúng thứ tự như trên
+   */
+  
 
 
 
