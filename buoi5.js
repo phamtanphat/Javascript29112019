349b06ba4306310b73c5b16da2b92f44d1a7de8f
phamtanphat <phatdroid94@gmail.com>
2019-12-09 03:28:23 -0800

    btap map

--- buoi5.js
+++ buoi5.js
@@ -1,5 +1,5 @@
 // Array map
-const numbers = [1,3,5,7,9]
+// const numbers = [1,3,5,7,9]
    /* const newNumbers = numbers.map(function(value , index , array){
         if (value >= 5){
             return value * 2
@@ -7,15 +7,45 @@ const numbers = [1,3,5,7,9]
         return value
     });
     console.log(newNumbers) */
-Array.prototype.myMap = function (fn){
-    var output = []
-    for(var i = 0 ; i < this.length ; i++){
-        const value = fn(this[i]);
-        output.push(value)
-    }
-    return output;
+// Array.prototype.myMap = function (fn){
+//     var output = []
+//     for(var i = 0 ; i < this.length ; i++){
+//         const value = fn(this[i]);
+//         output.push(value)
+//     }
+//     return output;
+// }
+// const newNumbers = numbers.myMap(function(value){
+//     return value * 2
+// })
+// console.log(newNumbers)
+
+function namesOnly(arr) {
+    const newArr = arr.map(function(object){
+        return object.name
+    })
+    return newArr;
 }
-const newNumbers = numbers.myMap(function(value){
-    return value * 2
-})
-console.log(newNumbers)
+console.log(namesOnly([
+  {
+    name: "Angelina Jolie",
+    age: 80
+  },
+  {
+    name: "Eric Jones",
+    age: 2
+  },
+  {
+    name: "Paris Hilton",
+    age: 5
+  },
+  {
+    name: "Kayne West",
+    age: 16
+  },
+  {
+    name: "Bob Ziroll",
+    age: 100
+  }
+]))
+// ["Angelina Jolie", "Eric Jones", "Paris Hilton", "Kayne West", "Bob Ziroll"]
