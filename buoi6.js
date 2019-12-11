8f7330c5dc8cacc9a9eb8dd18ff8dc2644f744dd
phamtanphat <phatdroid94@gmail.com>
2019-12-11 03:08:00 -0800

    built-in

---
+++ buoi6.js
@@ -0,0 +1,16 @@
+// built-in function
+// 5 - 10
+// const number = (Math.random() * 5 ) + 5 
+// Ham lam tron 
+// const number1 = 1.5
+// console.log(Math.round(number1))
+//Lam tron xuong
+// console.log(Math.floor(number1))
+//Lam tron len
+// console.log(Math.ceil(1.01))
+var a = 9
+var b = 6
+// Math.sqrt = tinh can bac 2
+// Math.abs  = tri tuyet doi
+// Math.max = tim so lon nhat
+console.log(Math.sqrt(a))
