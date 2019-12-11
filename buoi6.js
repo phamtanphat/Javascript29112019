b9907fdcbbf60ae053f2d0fb8d2d3a477a26ae97
phamtanphat <phatdroid94@gmail.com>
2019-12-11 03:16:45 -0800

    object

--- buoi6.js
+++ buoi6.js
@@ -8,9 +8,18 @@
 // console.log(Math.floor(number1))
 //Lam tron len
 // console.log(Math.ceil(1.01))
-var a = 9
-var b = 6
+// var a = 9
+// var b = 6
 // Math.sqrt = tinh can bac 2
 // Math.abs  = tri tuyet doi
 // Math.max = tim so lon nhat
-console.log(Math.sqrt(a))
+// console.log(Math.sqrt(a))
+
+//Handle Object
+const teo = {
+    name : 'Nguyen Van Teo',
+    age : 5
+}
+
+console.log(Object.keys(teo).length)
+
