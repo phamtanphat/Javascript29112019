7a9c4a1faf41e302c923de1760b0395d70d9d4b1
phamtanphat <phatdroid94@gmail.com>
2019-12-13 03:18:08 -0800

    arguments

--- buoi7.js
+++ buoi7.js
@@ -3,13 +3,33 @@
 // const newNumbers = numbers.map(value => value * 2)
 // console.log(newNumbers)
 
-var a = {
-    name : 'AAA',
-    run : function() {
-      var run2 = function(){
-        console.log(this.name)
-      }
-      run2.bind(this)()
-    }
-  }
-  a.run()
+// var a = {
+//     name : 'AAA',
+//     run : function() {
+//       var run2 = function(){
+//         console.log(this.name)
+//       }
+//       run2.bind(this)()
+//     }
+//   }
+//   a.run()
+
+//Template String
+// const sayhi = "say hi"
+// const tisayhi = `Ti ${sayhi}`
+// const teosayhi = `Teo ${sayhi}`
+
+// console.log(teosayhi)
+
+// var a = {
+//     0 : 'phat',
+//     1 : '10',
+//     length : 2
+// }
+  
+//   console.log(Array.from(a))
+
+function showName(){
+    console.log(typeof arguments)
+}
+showName("Teo","Ti","Tun")
